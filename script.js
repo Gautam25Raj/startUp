63243639a77bb2872272e67a1cceb80f43b1322c
Gautam Raj <63155224+Gautam25Raj@users.noreply.github.com>
2021-03-26 18:48:32 +0530

    update 1
    Changed every old code with new and they have better arrangement.

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
-const main = document.querySelector('.deco-1');
-const loadText = document.querySelector(".loading_text");
+const hamburger = document.querySelector(".menu");
+const hamburgerImage = document.querySelector(".menu_img");
+const navlinks = document.querySelector(".nav__bar");
+const links = document.querySelector(".nav");
 
-let load = 0;
-let interval = setInterval(blurring, 40);
+//Hamburger Button
+hamburger.addEventListener("click", () => {
+  navlinks.classList.toggle("open");
+  checkOpen();
+});
 
-const scale = (num, in_min, in_max, out_min, out_max) => {
-  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
-};
-
-function blurring() {
-  load++;
-  if (load > 99) {
-    clearInterval(interval);
-    main.style.filter = `blur(0px)`;
-    loadText.style.display = "none";
-  }
-
-  loadText.innerHTML = `${load}%`;
-  loadText.style.opacity = scale(load, 0, 100, 1, .2);
-  // main.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+function checkOpen() {
+  if (navlinks.classList.contains("open"))
+    hamburgerImage.src = "icon/menu-cross.svg";
+  else hamburgerImage.src = "icon/menu.svg";
 }
